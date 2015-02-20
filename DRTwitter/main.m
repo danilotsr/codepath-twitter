a133f5e86ee6408e6f7b87e2565130b07216ccc1
Danilo Resende <danilo.tsr@gmail.com>
2015-02-19 19:02:31 -0800

    Initial version with auto-generated code from XCode

---
+++ DRTwitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DRTwitter
+//
+//  Created by Danilo Resende on 2/19/15.
+//  Copyright (c) 2015 CodePath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
