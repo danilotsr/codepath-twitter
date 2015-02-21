1ae7b1f93b89d2b0ed4e27d900a74dfacf13faa9
Danilo Resende <danilo.tsr@gmail.com>
2015-02-21 12:44:10 -0800

    simplified the table cell, but retweeded line is not hidden properly

--- DRTwitter/Views/DRTweetCell.m
+++ DRTwitter/Views/DRTweetCell.m
@@ -11,15 +11,13 @@
 
 @interface DRTweetCell()
 
-
 @property (weak, nonatomic) IBOutlet UIImageView *retweetIcon;
-@property (weak, nonatomic) IBOutlet UILabel *retweetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *retweetedLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 @property (weak, nonatomic) IBOutlet UILabel *ownerNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *ownerHandleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
 @property (weak, nonatomic) IBOutlet UILabel *messageLabel;
-@property (retain, nonatomic) IBOutlet UIView *retweetedView;
 
 @property (nonatomic, assign) BOOL showRetweetedView;
 
@@ -43,19 +41,17 @@
     // Configure the view for the selected state
 }
 
-- (void)setTweet:(DRTweet *)tweet {
-    if (tweet.retweetSource) {
-        if (!self.retweetedView) {
-            self.retweetedView = [[UIView alloc] initWithFrame:CGRectMake(8, 8, 304, 14)];
-            self.retweetedView.backgroundColor = [UIColor lightGrayColor];
-            [self insertSubview:self.retweetedView atIndex:0];
-        }
-        tweet = tweet.retweetSource;
-    } else {
-        // FIXME: this cell would not be able to render a regular tweet in the future
-        [self.retweetedView removeFromSuperview];
-        self.retweetedView = nil;
+- (void)setTweet:(DRTweet *)rawTweet {
+    DRTweet *tweet = rawTweet;
+
+    if ([rawTweet wasRetweeded]) {
+        tweet = rawTweet.retweetSource;
+        self.retweetedLabel.text = [NSString stringWithFormat:@"%@ retweeted", rawTweet.user.name];
     }
+    self.retweetedLabel.hidden = ![rawTweet wasRetweeded];
+    self.retweetIcon.hidden = ![rawTweet wasRetweeded];
+    // FIXME: remove extra empty space when tweet was not retweeded
+
     self.messageLabel.text = tweet.text;
     self.ownerNameLabel.text = tweet.user.name;
     self.ownerHandleLabel.text = [NSString stringWithFormat:@"@%@", tweet.user.screenName];
@@ -65,25 +61,25 @@
     self.timestampLabel.text = [tweet shortCreatedAt];
 }
 
-- (void)updateConstraints {
-    [super updateConstraints];
-
-    if (self.retweetedView) {
-        UIImageView __weak *profileImageView = self.profileImageView;
-        UIView __weak *retweetedView = self.retweetedView;
-        NSDictionary *viewsDict = NSDictionaryOfVariableBindings(profileImageView, retweetedView);
-        NSMutableArray *constraints = [NSMutableArray array];
-        NSArray *visualConstraints = @[@"|-8-[retweetedView]-8-|", @"V:|-8-[retweetedView]", @"V:[retweetedView]-8-[profileImageView]"];
-        for (NSString *visualConstraint in visualConstraints) {
-            [constraints addObjectsFromArray: [NSLayoutConstraint
-                                               constraintsWithVisualFormat:visualConstraint
-                                               options:0
-                                               metrics:nil
-                                               views:viewsDict]];
-        }
-        [self addConstraints:constraints];
-    }
-    [self layoutIfNeeded];
-}
+//- (void)updateConstraints {
+//    [super updateConstraints];
+//
+//    if (self.retweetedView) {
+//        UIImageView __weak *profileImageView = self.profileImageView;
+//        UIView __weak *retweetedView = self.retweetedView;
+//        NSDictionary *viewsDict = NSDictionaryOfVariableBindings(profileImageView, retweetedView);
+//        NSMutableArray *constraints = [NSMutableArray array];
+//        NSArray *visualConstraints = @[@"|-8-[retweetedView]-8-|", @"V:|-8-[retweetedView]", @"V:[retweetedView]-8-[profileImageView]"];
+//        for (NSString *visualConstraint in visualConstraints) {
+//            [constraints addObjectsFromArray: [NSLayoutConstraint
+//                                               constraintsWithVisualFormat:visualConstraint
+//                                               options:0
+//                                               metrics:nil
+//                                               views:viewsDict]];
+//        }
+//        [self addConstraints:constraints];
+//    }
+//    [self layoutIfNeeded];
+//}
 
 @end
